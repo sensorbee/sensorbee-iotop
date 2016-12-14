444988f9136d070bf96d975d1a897610d8f3a959
TANAKA Daisuke <tanakad@preferred.jp>
2016-12-14 19:05:05 +0900

    add node status view
    not fit for terminal like "top" command

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
+# Under implementing!
+
+so interface and API will be changed
+
 # sensorbee-iotop
 
 path: `$GOPATH/src/github.com/sensorbee/sensorbee-iotop`
 
+**memo**
+
+this version, uses REST API, will be changed to use UDSF and websocket?
+
+**required**
+
+* SensorBee: https://github.com/disktnk/sensorbee/tree/node-api
+    * add `/node_status` API
+
+**run**
+
+```bash
+./sensorbee-iotop
+```
+
+To stop, press "Esc"
--- iotop_main.go
+++ iotop_main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"gopkg.in/sensorbee/sensorbee.v0/client"
+
 	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 	"gopkg.in/sensorbee/sensorbee.v0/server/config"
 	"gopkg.in/urfave/cli.v1"
-	"os"
-	"time"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "sensorbee iotop"
-	app.Usage = "Monitoring tool for I/O profiling"
+	app.Usage = "Monitoring tool for node I/O"
 	app.Version = "0.0.1"
 	app.Flags = CmdFlags
 	app.Action = Run
@@ -28,6 +33,11 @@ var CmdFlags = []cli.Flag{
 		Value:  fmt.Sprintf("http://localhost:%d/", config.DefaultPort),
 		Usage:  "the address of the target SensorBee server",
 		EnvVar: "SENSORBEE_URI",
+	},
+	cli.StringFlag{
+		Name:  "api-version",
+		Value: "v1",
+		Usage: "target API version",
 	},
 	cli.Float64Flag{
 		Name:  "d",
@@ -89,8 +99,11 @@ func draw(c *cli.Context) {
 	termbox.Clear(coldef, coldef)
 
 	// test
-	msgs := []string{"abc", "1234", "あいうえお"}
-	for i, msg := range msgs {
+	msgs, err := getNodeStatus(c)
+	if err != nil {
+		msgs = err.Error()
+	}
+	for i, msg := range strings.Split(msgs, "\n") {
 		tbprint(0, i, coldef, coldef, msg)
 	}
 	termbox.Flush()
@@ -101,4 +114,49 @@ func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 		termbox.SetCell(x, y, c, fg, bg)
 		x += runewidth.RuneWidth(c)
 	}
+}
+
+func getNodeStatus(c *cli.Context) (string, error) {
+	res, err := do(c, client.Get, "node_status", nil, "cannot access node status")
+	if err != nil {
+		return "", err
+	}
+
+	var js map[string]interface{}
+	res.ReadJSON(&js)
+	fmtRes, err := json.MarshalIndent(js, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s", fmtRes), nil
+}
+
+func newRequester(c *cli.Context) (*client.Requester, error) {
+	r, err := client.NewRequester(c.String("uri"), c.String("api-version"))
+	if err != nil {
+		return nil, fmt.Errorf("cannot create HTTP requester, %v", err)
+	}
+	return r, nil
+}
+
+func do(c *cli.Context, method client.Method, path string, body interface{},
+	baseErrMsg string) (*client.Response, error) {
+	req, err := newRequester(c)
+	if err != nil {
+		return nil, fmt.Errorf("%v, %v", baseErrMsg, err)
+	}
+	res, err := req.Do(method, path, body)
+	if err != nil {
+		return nil, fmt.Errorf("%v, %v", baseErrMsg, err)
+	}
+	if res.IsError() {
+		errRes, err := res.Error()
+		if err != nil {
+			return nil, fmt.Errorf("%v and failed to parse error information, %v",
+				baseErrMsg, err)
+		}
+		return nil, fmt.Errorf("%v: %v, %v: %v", baseErrMsg, errRes.Code,
+			errRes.RequestID, errRes.Message)
+	}
+	return res, nil
 }
